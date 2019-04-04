1d60f93eb9a60ab906f2c3e012fc0f42ec8194ba
moka-a <aud1220a@gmail.com>
2019-04-04 23:40:32 +0900

    드나 실시간 랭킹

---
+++ google_crawler.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+setInterval(function () {
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), listMajors);
+  });
+}.bind(this), 2000)
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1gl2vz2He50L9RMg2CpQV_VeKLBkc9cQ-267DxiEGINs',
+    range: 'sheet!A2:J',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      // Print columns A and E, which correspond to indices 0 and 4.
+      let users = []
+
+      rows.map((row) => {
+        const time = row[0]
+        const name = row[1]
+        const score = 
+          (row[2].substr(0, 1) == "4" ? 1 : 0) +
+          (row[3].substr(0, 1) == "2" ? 1 : 0) +
+          (row[4].substr(0, 1) == "3" ? 1 : 0) +
+          (row[5].substr(0, 1) == "4" ? 1 : 0) +
+          (row[6].substr(0, 1) == "4" ? 1 : 0) +
+          (row[7].substr(0, 1) == "2" ? 1 : 0) +
+          (row[8].substr(0, 1) == "3" ? 1 : 0) +
+          (row[9].substr(0, 1) == "2" ? 1 : 0)
+        users.push({time: time, name: name, score: score})
+      });
+
+      const sorted = users.sort((a, b) => { return b.score - a.score })
+      fs.writeFileSync("assets/json/rank.json", JSON.stringify(sorted, null, "  "))
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
---
+++ pages/quizzz.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app">
+    <div class="section section-header quiz-header">
+      <div class="container">
+        <div class="title">
+          DroidKnights Quiz !!!
+        </div>
+        <div class="title-sub">
+          1등에 도전해보세요
+        </div>
+      </div>
+    </div>
+
+    <div class="section-container">
+
+      <div v-for="(user, index) in ranks" :key="index" class="quiz-item">
+
+        <div class="info">
+          <div class="rank">{{ index + 1 }}위</div>
+          <div class="score">
+            {{ user.score }}점
+          </div>
+          <div class="name">
+            <span class="instalink">{{ user.name }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+  import * as moment from "moment"
+  import "moment-timezone"
+
+  let ranks = require("~/assets/json/rank.json")
+
+  export default {
+    async asyncData({
+      app: {
+        $axios
+      }
+    }) {
+      setInterval(function () {
+        ranks = require("~/assets/json/rank.json")
+      }.bind(this), 2000)
+      return {
+      }
+    },
+    data() {
+      return {
+        ranks: ranks.filter(item => item.name != "")
+      }
+    },
+    mounted() {},
+    methods: {
+    },
+  }
+</script>
